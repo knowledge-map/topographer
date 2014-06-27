5b1e0b990107f8c2a62a69ca1ee7746acf0594ab
Kenni Bawden <kenni.bawden@gmail.com>
2014-06-27 13:33:52 +1000

    Moved JS to a separate file

---
+++ website/js/topographer.js
@@ -0,0 +1,95 @@
+Parse.initialize("HCqG8ipL0wyGCrMl60mKk1w9EqWTfmUl2k05pnJm", "GX0soAeJUyTHePsm83pcE95kydFz7y55mplHSd1A");
+
+var Resource = Parse.Object.extend("Resource");
+var Concept = Parse.Object.extend("Concept");
+var ConceptMap = Parse.Object.extend("ConceptMap");
+var ConceptRelationship = Parse.Object.extend("ConceptRelationship");
+
+function saveResource(form) {
+    var resource = new Resource();
+
+    resource.save({
+        name: form.name.value,
+        URL: form.URL.value,
+    }, {
+        success: function() {console.log("success", arguments);},
+        error: function() {console.log("failure", arguments);}
+    });
+
+    var relationships = ["teaches", "requires"];
+    relationships.forEach(function(relationship) {
+        var conceptTags = $(form[relationship]).tagit("assignedTags");
+        saveMappings(resource, relationship, conceptTags);
+    });
+
+    return false;
+}
+
+function saveMappings(resource, relationshipName, conceptNames) {
+    var concepts = getConcepts(conceptNames);
+    var relationship = getRelationship(relationshipName);
+    var maps = getConceptMaps(resource);
+
+    Parse.Promise.when([concepts, relationship, maps])
+    .then(function(concepts, relationship, maps) {
+        console.log(maps);
+        maps.forEach(function(map) {
+            map.destroy();
+        });
+
+        conceptNames.forEach(function(conceptName) {
+            var existingConcepts = concepts.filter(function(concept) {
+                return concept.get("name") == conceptName;
+            });
+
+            if (existingConcepts.length > 0) {
+                existingConcepts.forEach(function(concept) {
+                    saveMapping(resource, relationship, concept);
+                });
+            } else {
+                var concept = new Concept();
+                concept.save({name: conceptName})
+                .then(function(concept) {
+                    saveMapping(resource, relationship, concept);
+                });
+            }
+        });
+    });
+}
+
+function saveMapping(resource, relationship, concept) {
+    var conceptMap = new ConceptMap();
+
+    conceptMap.save({
+        resource: resource,
+        relationship: relationship,
+        concept: concept,
+    });
+}
+
+function getConcepts(conceptNames) {
+    var query = new Parse.Query(Concept);
+    query.containedIn("name", conceptNames);
+
+    return query.find();
+}
+
+function getConceptMaps(resource) {
+    var query = new Parse.Query(ConceptMap);
+    if (resource.isNew()) {
+        return Parse.Promise.as([]);
+    } else {
+        query.equalTo("resource", resource);
+        return query.find();
+    }
+}
+
+function getRelationship(relationshipName) {
+    var query = new Parse.Query(ConceptRelationship);
+    query.equalTo("name", relationshipName)
+
+    return query.find()
+        .then(function(relationships) {
+            return relationships.length > 0 ? relationships[0] : null;
+        });
+}
